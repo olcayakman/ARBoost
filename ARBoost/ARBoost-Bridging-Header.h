ed2da0f37a92ab76bba0f5628a92953c9e41c1ec
Mert Gökçen <mertgursugokcen@gmail.com>
2021-08-05 08:16:17 +0300

    add card.io files

---
+++ ARBoost/ARBoost-Bridging-Header.h
@@ -0,0 +1,19 @@
+//
+//  ARBoost-Bridging-Header.h
+//  ARBoost
+//
+//  Created by Mert Gökçen on 5.08.2021.
+//
+
+#ifndef ARBoost_Bridging_Header_h
+#define ARBoost_Bridging_Header_h
+
+#import "CardIO.h"
+
+@import AudioToolbox;
+@import AVFoundation;
+@import CoreMedia;
+@import CoreVideo;
+@import MobileCoreServices;
+
+#endif /* ARBoost_Bridging_Header_h */
