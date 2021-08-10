da33b14e337d34b8179bfd32a2daf92b9fc7f40b
Mert Gökçen <mertgursugokcen@gmail.com>
2021-08-10 09:09:05 +0300

    Adding text to the first table

--- ARBoost/Controller/ARViewController.swift
+++ ARBoost/Controller/ARViewController.swift
@@ -30,20 +30,34 @@ class ARViewController: UIViewController, ARSCNViewDelegate {
         // Set the scene to the view
         //sceneView.scene = scene
         
-        //To add the first table 
+        sceneView.scene.rootNode.addChildNode(loadFirstTable())
+        sceneView.autoenablesDefaultLighting = true
+        
+    }
+    
+    func loadFirstTable() -> SCNNode{
         let tableMaterial = SCNMaterial()
-        tableMaterial.diffuse.contents = UIImage(named:"art.scnassets/temel.png")
-        let rectangle = SCNBox(width: 0.2, height: 0.2, length: 0, chamferRadius: 0.01)
+        
+        let img = textToImage(drawText: "Hoşgeldin", inImage: UIImage(named:"art.scnassets/temel.png")!, atPoint: CGPoint(x: 200, y: 80))
+        
+        let img2 = textToImage(drawText: "Mert Gökçen", inImage: img, atPoint: CGPoint(x: 200, y: 110))
+        
+        let img3 = textToImage(drawText: "Kart Borcu", inImage: img2, atPoint: CGPoint(x: 150, y: 150))
+        
+        let img4 = textToImage(drawText: "160TL", inImage: img3, atPoint: CGPoint(x: 160, y: 200))
+        
+        let img5 = textToImage(drawText: "Son İşlem", inImage: img4, atPoint: CGPoint(x: 150, y: 250))
+        
+        let img6 = textToImage(drawText: "333TL", inImage: img5, atPoint: CGPoint(x: 180, y: 350))
+        
+        tableMaterial.diffuse.contents = img6
+        let rectangle = SCNBox(width: 0.3, height: 0.3, length: 0, chamferRadius: 0.01)
         rectangle.materials = [tableMaterial]
         let tableNode = SCNNode()
         tableNode.position = SCNVector3(x: 0, y: 0.1, z: -0.5)
         tableNode.geometry = rectangle
-        sceneView.scene.rootNode.addChildNode(tableNode)
         
-        
-        
-        
-        sceneView.autoenablesDefaultLighting = true
+        return tableNode
         
     }
     
@@ -88,5 +102,29 @@ class ARViewController: UIViewController, ARSCNViewDelegate {
     func sessionInterruptionEnded(_ session: ARSession) {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
         
+    }
+    
+    
+    func textToImage(drawText text: String, inImage image: UIImage, atPoint point: CGPoint) -> UIImage {
+        let textColor = UIColor.systemRed
+            //UIColor(red: CGFloat(21), green: CGFloat(57), blue: CGFloat(18), alpha: CGFloat(0))
+        let textFont = UIFont(name: "Helvetica Bold", size: 16)!
+
+        let scale = UIScreen.main.scale
+        UIGraphicsBeginImageContextWithOptions(image.size, false, scale)
+
+        let textFontAttributes = [
+            NSAttributedString.Key.font: textFont,
+            NSAttributedString.Key.foregroundColor: textColor,
+            ] as [NSAttributedString.Key : Any]
+        image.draw(in: CGRect(origin: CGPoint.zero, size: image.size))
+
+        let rect = CGRect(origin: point, size: image.size)
+        text.draw(in: rect, withAttributes: textFontAttributes)
+
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+        return newImage!
     }
 }
