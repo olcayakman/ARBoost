da2479d31307cd66862563c65cd2df6300871719
Mert Gökçen <mertgursugokcen@gmail.com>
2021-08-19 16:32:38 +0300

    Start of account backend requests

---
+++ ARBoost/Model/Account.swift
@@ -0,0 +1,8 @@
+//
+//  Account.swift
+//  ARBoost
+//
+//  Created by Mert Gökçen on 19.08.2021.
+//
+
+import Foundation
---
+++ ARBoost/Model/AccountNetworkHandler.swift
@@ -0,0 +1,8 @@
+//
+//  AccountNetworkHandler.swift
+//  ARBoost
+//
+//  Created by Mert Gökçen on 19.08.2021.
+//
+
+import Foundation
